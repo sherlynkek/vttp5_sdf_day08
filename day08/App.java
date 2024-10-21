de6ae3a05e129cc85f77c1de48d3ee161fefe747
sherlynkek <sherlynkek00@gmail.com>
2024-10-21 09:26:26 +0800

    first commit

---
+++ day08/App.java
@@ -0,0 +1,7 @@
+package day08;
+
+public class App {
+    public void main(String[] args) {
+        
+    }
+}
---
+++ day08/Product.java
@@ -0,0 +1,76 @@
+package day08;
+
+import java.util.Date;
+
+public class Product {
+    Long id;
+    String prodName;
+    String prodDesc;
+    String prodCat;
+    Float price;
+    Date createdDate;
+    
+
+    public Product(Long id, String prodName, String prodDesc, String prodCat, Float price, Date createdDate) {
+        this.id = id;
+        this.prodName = prodName;
+        this.prodDesc = prodDesc;
+        this.prodCat = prodCat;
+        this.price = price;
+        this.createdDate = createdDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getProdDesc() {
+        return prodDesc;
+    }
+
+    public void setProdDesc(String prodDesc) {
+        this.prodDesc = prodDesc;
+    }
+
+    public String getProdCat() {
+        return prodCat;
+    }
+
+    public void setProdCat(String prodCat) {
+        this.prodCat = prodCat;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [toString()=" + super.toString() + "]";
+    }  
+    
+}
